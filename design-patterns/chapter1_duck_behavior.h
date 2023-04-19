e43a3696654e2f03b79af72d5210a64b0bf81c48
김동유 <ehddb2252@gmail.com>
2023-04-20 01:28:03 +0900

    refactor: add virtual keyword

--- design-patterns/chapter1_duck_behavior.h
+++ design-patterns/chapter1_duck_behavior.h
@@ -3,21 +3,21 @@
 
 namespace chapter1 {
 	class FlyWithWings : public FlyBehavior {
-		void PerformFly() override;
+		virtual void PerformFly() override;
 	};
 	class FlyNoWay : public FlyBehavior {
-		void PerformFly() override;
+		virtual void PerformFly() override;
 	};
 	class FlyRocketPowered : public FlyBehavior {
-		void PerformFly() override;
+		virtual void PerformFly() override;
 	};
 	class Quack : public QuackBehavior {
-		void PerformQuack() override;
+		virtual void PerformQuack() override;
 	};
 	class Squeak : public QuackBehavior {
-		void PerformQuack() override;
+		virtual void PerformQuack() override;
 	};
 	class MuteQuack : public QuackBehavior {
-		void PerformQuack() override;
+		virtual void PerformQuack() override;
 	};
 }
